e12a405cac80916c3228dee61412762344790f83
keunbum <rkeunbum@gmail.com>
2022-05-24 00:41:43 +0900

    hard coding changes to file io in ItemManager

--- src/domain/product/ItemManager.java
+++ src/domain/product/ItemManager.java
@@ -1,32 +1,39 @@
 package domain.product;
 import domain.payment.*;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Scanner;
+
 public class ItemManager {
     private static final int MAX_ITEM = 20;
-    private static final String[] drinkList = {"콜라", "사이다",
-            "녹차", "홍차",
-            "밀크티", "탄산수",
-            "보리차", "캔커피",
-            "물", "에너지드링크",
-            "바닷물", "식혜",
-            "아이스티", "딸기주스",
-            "오렌지주스", "포도주스",
-            "이온음료", "아메리카노",
-            "핫초코", "카페라떼"
-    };
-    private static final int[] prices = {100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100,100};
-    private static final int[] quantities = {999,999,999,999,999,999,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-    Item[] items = new Item[MAX_ITEM];
+    private static Item[] items;
 
     public ItemManager() {
-        for (int i = 0; i < MAX_ITEM; i++) {
-            items[i] = new Item(i, quantities[i], drinkList[i], prices[i]);
+        loadItemList();
+    }
+
+    private void loadItemList() {
+        InputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream("src/domain/product/itemList.txt");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        assert inputStream != null;
+        Scanner in = new Scanner(inputStream);
+        int i = 0;
+        items = new Item[MAX_ITEM];
+        while (in.hasNext()) {
+            String[] argv = in.nextLine().split(" ", 3);
+            items[i++] = new Item(i, argv[0], Integer.parseInt(argv[1]), Integer.parseInt(argv[2]));
         }
     }
 
     public boolean checkStock(int itemId, int itemQuantity) {
         for (int i = 0; i < MAX_ITEM; i++) {
-            if (items[i] != null && items[i].getItemId() == itemId) {
+            if (items[i].getItemId() == itemId) {
                 return items[i].getItemQuantity() >= itemQuantity;
             }
         }
@@ -39,25 +46,26 @@ public class ItemManager {
 
     public void updateQuantity(int itemId, int itemQuantity) {
         for (int i = 0; i < MAX_ITEM; i++) {
-            if (items[i] != null && items[i].getItemId() == itemId) {
+            if (items[i].getItemId() == itemId) {
                 items[i].setItemQuantity(items[i].getItemQuantity() + itemQuantity);
-                break;
+                return;
             }
         }
     }
 
     public boolean checkProduct(int itemId) {
         for (int i = 0; i < MAX_ITEM; i++) {
-            if (items[i] != null && items[i].getItemId() == itemId) {
+            if (items[i].getItemId() == itemId) {
                 return true;
             }
         }
         return false;
     }
 
-    public void synchronize(int itemId, int itemQuantity, String verification) {
-        boolean verificationValidity = false;
+    /* TODO: has to be modified due to race conditions. */
+    public void synchronize(int itemId, int itemQuantity, String verificationCode) {
         VerificationManager v = new VerificationManager();
+        boolean verificationValidity = false;
         for (int i = 0; i < MAX_ITEM; i++) {
             if (items[i] != null && items[i].getItemId() == itemId) {
                 if (items[i].getItemQuantity() >= itemQuantity) {
@@ -67,6 +75,10 @@ public class ItemManager {
                 break;
             }
         }
-        v.saveVerification(itemId, itemQuantity, verification, verificationValidity);
+        v.saveVerification(itemId, itemQuantity, verificationCode, verificationValidity);
+    }
+
+    public Item[] getItemList() {
+        return items;
     }
 }
