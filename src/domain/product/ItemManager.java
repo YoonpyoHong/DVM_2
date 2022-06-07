f79975da5b48282e2c2db656ed5dfb74b2de0cdb
HazirahMuslim <58877998+HazirahMuslim@users.noreply.github.com>
2022-06-08 02:57:54 +0900

    ui debug fixed

--- src/domain/product/ItemManager.java
+++ src/domain/product/ItemManager.java
@@ -55,6 +55,7 @@ public class ItemManager {
     private void loadItemList() {
         InputStream inputStream = null;
         try {
+             inputStream = new FileInputStream("src/domain/product/itemList.txt");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
