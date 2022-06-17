9202920e96f042523a6a78de0906413bcea823de
keunbum <rkeunbum@gmail.com>
2022-06-17 20:42:11 +0900

    create Order Class

---
+++ src/domain/payment/Order.java
@@ -0,0 +1,21 @@
+package domain.payment;
+
+import domain.product.Item;
+
+public class Order {
+    private Item item;
+    private int quantity;
+
+    public Order(Item item, int quantity) {
+        this.item = item;
+        this.quantity = quantity;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+}
