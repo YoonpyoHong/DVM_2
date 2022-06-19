ed2e82a1bd50b421ccc0bba5bea84072851ec314
HazirahMuslim <hazirahcrystal@gmail.com>
2022-06-19 18:50:37 +0900

    update AdminWindow

--- src/ui/AdminWindow.java
+++ src/ui/AdminWindow.java
@@ -23,7 +23,7 @@ public class AdminWindow extends DvmPanel {
     private static final int DRINK_PANEL_WIDTH = 350;
     private static final int DRINK_PANEL_HEIGHT = 400;
 
-    private static final int DRINK_PANEL2_WIDTH = 150;
+    private static final int DRINK_PANEL2_WIDTH = 100;
     private static final int DRINK_PANEL2_HEIGHT = 250;
 
     private static final int DRINK_PANEL3_WIDTH = 200;
@@ -53,16 +53,17 @@ public class AdminWindow extends DvmPanel {
         JPanel itemLayout = new JPanel();
         itemLayout.setPreferredSize(new Dimension(DRINK_PANEL_WIDTH, DRINK_PANEL_HEIGHT));
         itemLayout.setBorder(BorderFactory.createCompoundBorder(grayLineBorder, emptyBorder));
-        addComponent(mainPanel,itemLayout, 0, 6, 0, 10, 0, 1, 3, 10, GridBagConstraints.LINE_START);
+        addComponent(mainPanel,itemLayout, 0, 6, 0, 10, 0, 1, 3, 10, GridBagConstraints.PAGE_START);
 
         JPanel itemLayout2 = new JPanel();
         itemLayout2.setPreferredSize(new Dimension(DRINK_PANEL2_WIDTH, DRINK_PANEL2_HEIGHT));
-        addComponent(mainPanel,itemLayout2, 0, 0, 0, 125, 0, 0, 0, 0, GridBagConstraints.LINE_START);
+        addComponent_test(itemLayout,itemLayout2);
+//            addComponent(mainPanel,itemLayout2, 0, 0, 0, 125, 0, 0, 0, 0, GridBagConstraints.LINE_END);
 
         JPanel itemLayout3 = new JPanel();
         itemLayout3.setPreferredSize(new Dimension(DRINK_PANEL3_WIDTH, DRINK_PANEL3_HEIGHT));
-        addComponent(mainPanel,itemLayout3, 0, 15, 0, 0, 0, 0, 0, 0, GridBagConstraints.LINE_START);
-
+        addComponent_test(itemLayout,itemLayout3);
+//        addComponent(mainPanel,itemLayout3,0, 15, 0, 0, 0, 0, 0, 0, GridBagConstraints.LINE_START);
 
         JButton logoutBtn = new JButton("LOGOUT");
         logoutBtn.setFocusable(false);
@@ -74,10 +75,13 @@ public class AdminWindow extends DvmPanel {
         updateBtn.addActionListener(this);
         addComponent(mainPanel, updateBtn, 10, 2, 2, 10, 4, 4, 0.5, GridBagConstraints.LINE_END);
 
-        initButtons(itemLayout2);
         initTextFields(itemLayout3);
+        initDrinkList(itemLayout2);
     }
 
+    private  void addComponent_test(JPanel panel, Component component){
+        panel.add(component, BorderLayout.CENTER);
+    }
     public void actionPerformed(ActionEvent e) {
         if (e.getActionCommand().equals("LOGOUT")) {
             resetCard();
@@ -117,15 +121,15 @@ public class AdminWindow extends DvmPanel {
         }
     }
 
-    private void initButtons(JPanel panel) {
+    private void initDrinkList(JPanel panel) {
         for (int i = 0; i < ItemManager.MAX_LOCAL_ITEM; i++) {
-            JLabel[] btn = new JLabel[ItemManager.MAX_LOCAL_ITEM];
-            btn[i] = new JLabel(localItems[i].getItemName(), SwingConstants.CENTER);
-            btn[i].setPreferredSize(new Dimension(BUTTON_WIDTH, BUTTON_HEIGHT));
-            btn[i].setOpaque(true);
-            btn[i].setBackground(Color.WHITE);
-            btn[i].setBorder(BorderFactory.createLineBorder(Color.decode("#cfd0d1"), 1));
-            panel.add(btn[i], BorderLayout.CENTER);
+            JLabel[] drinkName = new JLabel[ItemManager.MAX_LOCAL_ITEM];
+            drinkName[i] = new JLabel(localItems[i].getItemName(), SwingConstants.CENTER);
+            drinkName[i].setPreferredSize(new Dimension(BUTTON_WIDTH, BUTTON_HEIGHT));
+            drinkName[i].setOpaque(true);
+            drinkName[i].setBackground(Color.WHITE);
+            drinkName[i].setBorder(BorderFactory.createLineBorder(Color.decode("#cfd0d1"), 1));
+            panel.add(drinkName[i], BorderLayout.CENTER);
         }
     }
 
